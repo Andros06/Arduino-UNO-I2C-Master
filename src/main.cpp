2b025d8b7ebfb302c2837f45b5ceeb813f00a3a2
Andreas Roset <andreasroset@gmail.com>
2024-03-14 09:06:27 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+// Lager variabler for x og y
 int x = 0;
+int y = 0;
+
 void setup() {
-  // Start the I2C Bus as Master
+  // Starter I2C Bus as Master
   Wire.begin(); 
+
+  // Starter Seriel med pc
   Serial.begin(9600);
 }
+
 void loop() {
-  Wire.beginTransmission(9); // transmit to device #9
-  Wire.write(x);              // sends x 
-  Wire.endTransmission();    // stop transmitting
-  x++; // Increment x
-  if (x > 5) x = 0; // `reset x once it gets 6
+  Wire.beginTransmission(9); // Starter overføring til addresse 9
+  Wire.write(x);              // Sender x verdi 
+  Serial.print("Sender X verdi til Mega: ");
   Serial.println(x);
+  Wire.endTransmission();    // Stopper overføring til addresse 9
+
+  delay(100); //Liten delay for meldinger
+
+  Wire.requestFrom(9, 1); // Spør om 1 byte fra addresse 9
+  if(Wire.available()){
+    y = Wire.read(); // Setter y verdi til lest verdi fra melding
+    Serial.print("Y verdi fra MEGA: ");
+    Serial.println(y);
+  }
+
+  x++; // Legger på x verdi for å endre den over tid
+  if (x > 5) x = 0; // Reseter x verdi slik at den går i loop
+  
   delay(500);
 }
